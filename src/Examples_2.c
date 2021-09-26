cbd668dfb0b2cca556979690667dde6269288a6d
chadd williams <will4614@pacificu.edu>
2021-09-26 14:56:23 -0700

    add example2 code

--- src/Examples_2.c
+++ src/Examples_2.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <string.h> 
 #include <stdlib.h>
+#include <ctype.h>
 
 /****************************************************************************
  Function:		main
@@ -22,5 +23,125 @@
 
 int main()
 {
+  const char* RO_STR = "Hello CS 360. Max: 28 Students\nHave fun!\n\n";
+  const int BUF_SIZE = 1024;
+  const int SM_BUF_SIZE = 15;
+
+  char szBuffer[BUF_SIZE];
+  char szSmBuffer[SM_BUF_SIZE];
+  char *pStr;
+  char *pEnd;
+
+  int value;
+  char tmpChar;
+
+
+  // set all of szSmBuffer to '\0'
+  memset(szSmBuffer, '\0', SM_BUF_SIZE);
+
+  // use strncat to copy the first 13 bytes
+  // from RO_STR to szSmBuffer
+
+  // NOTE: this strncpy does NOT put a \0 
+  // at the end of the data. We had to 
+  // put the \0 using memset!
+  // also, we know 13 < SM_BUF_SIZE
+  strncpy(szSmBuffer, RO_STR, 13);
+
+  // szSmBuffer now looks like:
+  // Hello CS 360.\0\0
+  printf("szSmBuffer: >|%s|<\n\n", szSmBuffer);
+
+  // use strncpy to copy RO_STR into
+  // szBuffer
+
+  // copy at most BUF_SIZE bytes from RO_STR
+  // to szBuffer.
+  // Since the \0 in RO_STR happens in 
+  // the first BUF_SIZE bytes, the \0 is
+  // copied to szBuffer.
+  strncpy(szBuffer, RO_STR, BUF_SIZE);
+
+  printf("szBuffer >|%s|<\n\n", szBuffer);
+
+  // use strncat to copy RO_STR on to
+  // the end of szBuffer
+
+  strncat(szBuffer, RO_STR, BUF_SIZE - strlen(szBuffer) - 1);
+
+  printf("szBuffer >|%s|<\n\n", szBuffer);
+
+  // use strstr to find Max: in szBuffer
+  pStr = strstr(szBuffer, "Max:");
+
+  if ( NULL == pStr )
+  {
+    printf("Max: NOT FOUND!\n\n");
+  }
+  else
+  {
+    // found Max:
+    // pStr now points to the point in szBuffer
+    // that contains the first Max:
+    printf("pStr >|%s|<\n\n", pStr);
+
+    // find the first digit past Max:
+    while ( !isdigit(*pStr))
+    {
+      ++ pStr;
+    }
+
+    // use pEnd to walk the string to
+    // find the end of the digits '28'.
+    pEnd = pStr;
+
+    while ( isdigit(*pEnd))
+    {
+      ++ pEnd;
+    } 
+
+    // Now pStr points to the first digit in 28
+    // and pEnd points to the first character past
+    // the last digit. In this case, a space.
+
+    // print the char pStr points to, so use the *
+    printf("pStr >|%c|< pEnd >|%c|<\n\n", *pStr, *pEnd);
+
+    // turn the text into an int using atoi
+    // atoi requires a NULL terminated string
+    // that only contains digits.
+
+    // We need to NULL terminate the digits
+    // by replacing the space with a \0.
+    // Let's save the space in a temp char
+    tmpChar = *pEnd;
+
+    // replace the space with at NULL 
+    *pEnd = '\0';
+
+    // Now, the string that starts at pStr
+    // is NULL terminated. 
+    // Let's check. We know printf prints
+    // until a \0 is found.
+    printf("pStr >|%s|<\n\n", pStr);
+
+    // Let's check szBuffer too!
+    printf("szBuffer >|%s|<\n\n", szBuffer);
+
+    // now call atoi
+    value = atoi(pStr);
+
+    // restore the space
+    *pEnd = tmpChar;
+
+    // print the int!
+    printf("value: >|%d|<\n\n", value);
+
+    // print szBuffer after restoring the space
+    printf("szBuffer >|%s|<\n\n", szBuffer);
+  }
+
+
+
   return EXIT_SUCCESS;
 }
